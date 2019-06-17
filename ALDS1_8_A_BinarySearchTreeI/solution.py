7b0106554680434d60a2fcdd9c2916feffa7552a
fujiki <abcdef.nakamura@gmail.com>
2019-06-17 10:05:21 +0900

    add solution to binary search tree: insert - Case #5 Time Limit Exceeded

---
+++ ALDS1_8_A_BinarySearchTreeI/solution.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, key=-1, parent=None, left=None, right=None):
+        self.key = key
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
+class Tree:
+    def __init__(self, ):
+        self.root = None
+        self.node_list = []
+
+    def insert(self, node):
+        y = None
+        x = self.root
+        while x is not None:
+            y = x
+            if node.key < y.key:
+                x = x.left
+            else:  # node.key >= y.key
+                x = x.right
+
+        node.parent = y
+        if y is None:
+            self.root = node
+        elif node.key < y.key:
+            y.left = node
+        else:
+            y.right = node
+
+    def inorder(self, node):
+        if node is None: return  # noqa
+        self.inorder(node.left)
+        self.node_list.append(node)
+        self.inorder(node.right)
+        keys = [node.key for node in self.node_list]
+        return keys
+
+    def preorder(self, node):
+        if node is None: return # noqa
+        self.node_list.append(node)
+        self.preorder(node.left)
+        self.preorder(node.right)
+        keys = [node.key for node in self.node_list]
+        return keys
+
+
+if __name__ == '__main__':
+    tree = Tree()
+    m = int(input())
+    for _ in range(m):
+        order, *k = input().split()
+        if order.startswith('insert'):
+            k = int(k.pop())
+            node = Node(key=k)
+            tree.insert(node)
+        elif order.startswith('print'):
+            tree.node_list = []
+            node_list = tree.inorder(tree.root)
+            print(' ' + ' '.join(map(str, node_list)))
+            tree.node_list = []
+            node_list = tree.preorder(tree.root)
+            print(' ' + ' '.join(map(str, node_list)))
